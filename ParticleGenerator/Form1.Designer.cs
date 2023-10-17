f2497760d2aa3ecd9497ac573c0eff762dcbbe34
akasunic <amkasunic@gmail.com>
2023-10-17 14:12:21 -0400

    form design for steady particles
    basic design only

--- ParticleGenerator/Form1.Designer.cs
+++ ParticleGenerator/Form1.Designer.cs
@@ -29,32 +29,354 @@
     private void InitializeComponent()
     {
             this.genSteadyParts = new System.Windows.Forms.Button();
+            this.chooseProjectFileDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveQuillProjectDialog = new System.Windows.Forms.SaveFileDialog();
+            this.selectQuillButton = new System.Windows.Forms.Button();
+            this.layersComboBox = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
             this.SuspendLayout();
             // 
             // genSteadyParts
             // 
-            this.genSteadyParts.Location = new System.Drawing.Point(279, 262);
+            this.genSteadyParts.Location = new System.Drawing.Point(281, 353);
             this.genSteadyParts.Name = "genSteadyParts";
             this.genSteadyParts.Size = new System.Drawing.Size(174, 23);
             this.genSteadyParts.TabIndex = 0;
-            this.genSteadyParts.Text = "Generate Particles";
+            this.genSteadyParts.Text = "Generate Steady Particles";
             this.genSteadyParts.UseVisualStyleBackColor = true;
             this.genSteadyParts.Click += new System.EventHandler(this.genParts_click);
+            // 
+            // selectQuillButton
+            // 
+            this.selectQuillButton.Location = new System.Drawing.Point(92, 58);
+            this.selectQuillButton.Name = "selectQuillButton";
+            this.selectQuillButton.Size = new System.Drawing.Size(209, 23);
+            this.selectQuillButton.TabIndex = 1;
+            this.selectQuillButton.Text = "Select Quill project folder";
+            this.selectQuillButton.UseVisualStyleBackColor = true;
+            this.selectQuillButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // layersComboBox
+            // 
+            this.layersComboBox.FormattingEnabled = true;
+            this.layersComboBox.Location = new System.Drawing.Point(536, 58);
+            this.layersComboBox.Name = "layersComboBox";
+            this.layersComboBox.Size = new System.Drawing.Size(221, 23);
+            this.layersComboBox.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(281, 236);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number of drawing duplicates";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Number of particle offsets";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(281, 296);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown2.TabIndex = 6;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown3.Location = new System.Drawing.Point(288, 114);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown3.TabIndex = 8;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "X-axis/length, as scale of Quill grid";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown4.Location = new System.Drawing.Point(288, 143);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown4.TabIndex = 10;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(92, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(190, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Y-axis/height, as scale of Quill grid";
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown5.Location = new System.Drawing.Point(288, 172);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown5.TabIndex = 12;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Z-axis/depth, as scale of Quill grid";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(92, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(216, 19);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Base area for random particles";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(321, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(381, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Default values of 1 produce a cubic area with faces the size of Quill grid";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(92, 213);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(462, 19);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Number of drawing duplicates from original layer (within that area)";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(92, 273);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(433, 19);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Number of times to duplicate and offset particles for animation";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(344, 62);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(186, 15);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Layer containing particle drawing:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(229, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(314, 25);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Steady Particles Generator for Quill";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.layersComboBox);
+            this.Controls.Add(this.selectQuillButton);
             this.Controls.Add(this.genSteadyParts);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Steady Particles Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
     #endregion
 
     private Button genSteadyParts;
+    private FolderBrowserDialog chooseProjectFileDialog;
+    private SaveFileDialog saveQuillProjectDialog;
+    private Button selectQuillButton;
+    private ComboBox layersComboBox;
+    private NumericUpDown numericUpDown1;
+    private Label label1;
+    private Label label2;
+    private NumericUpDown numericUpDown2;
+    private NumericUpDown numericUpDown3;
+    private Label label3;
+    private NumericUpDown numericUpDown4;
+    private Label label4;
+    private NumericUpDown numericUpDown5;
+    private Label label5;
+    private Label label6;
+    private Label label7;
+    private Label label8;
+    private Label label9;
+    private Label label10;
+    private Label label11;
   }
 }
--- ParticleGenerator/Form1.cs
+++ ParticleGenerator/Form1.cs
@@ -38,5 +38,45 @@ namespace ParticleGenerator
       steadyParticles.GenerateSteadyParticles();
       QuillSequenceWriter.Write(sequence, writePath);
     }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label3_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label7_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label8_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label11_Click(object sender, EventArgs e)
+    {
+
+    }
   }
 }
